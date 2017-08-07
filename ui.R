08a655308879939ac6ef1e70859b513860847cdb
Eli Nicholson <efn3@hood.edu>
2017-08-07 13:41:42 -0400

    Update ui.R

--- ui.R
+++ ui.R
@@ -21,15 +21,17 @@ vars <- c(
   "Fumbles Lost" = "FumblesLost",
   "Turn Overs" = "TurnOvers",
   "Superbowl Wins" = "SuperbowlWin",
-  "Superbowl Losses" = "SuperbowlLosse",
+  "Superbowl Losses" = "SuperbowlLoss",
   "Superbowl Percent Win" = "SuperbowlPercent",
   "Superbowl Points" = "SuperbowlPoints",
   "Superbowl Opposing Points" = "SuperbowlOpposingPoints")
 
+
 shinyUI(fluidPage(
   
   navbarPage("NFL 2016 Data in ColoR", id = "nav",
-             tabPanel("Interactive Data MapR",
+             
+             tabPanel("Interactive Data Map",
                       tags$head(
                         # Include our custom CSS
                         includeCSS("solarized.css")
@@ -44,8 +46,6 @@ shinyUI(fluidPage(
                           ),
                           
                           leafletOutput("map", width="100%", height="100%"),
-                          
-                          # Shiny versions prior to 0.11 should use class="modal" instead.
                           absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
                                         draggable = TRUE, top = 60, left = "auto", right = 20, bottom = "auto",
                                         width = 330, height = "auto",
@@ -53,23 +53,39 @@ shinyUI(fluidPage(
                                         h2("Explorer"),
                                         
                                         selectInput("size","Size", vars, selected = "Rank"),
-                                        
+
                                         selectInput("color","Color", vars, selected = "PointsPerGame"),
-                                        
+
                                         plotOutput("scatterNFL2016", height = 200),
-                                        
+
                                         plotOutput("superbowlhist", height = 250),
-                                        
+
                                         actionButton("reset_button", "Reset View")
-                                        
-                                        
-                                        
-                                        
-                          )
+                          )               
                       )
              ),
              
-             tabPanel("Team Data FindR",
+             
+             
+             tabPanel("PlotR",
+                      fluidRow(
+                        column(4,
+                               selectInput("xaxis","X-Axis", vars, selected = "Rank")
+                        ),
+                        column(4,
+                               selectInput("yaxis", "Y-Axis", vars, selected = "Rank")
+                        ),
+                        column(2,
+                               radioButtons("cluster","Cluster Choice", choices = list("none","Conference","Division"), selected = "none")),
+                        column(2,
+                               radioButtons("regression", "Regression Type",choices = c("none","linear","loess"), selected = "none"))
+                      ),
+                      plotOutput("InteractiveScatter"),
+                      
+                      h3(textOutput("correlation"), align = "center")
+             ),
+             
+             tabPanel("Data",
                       fluidRow(
                         column(3,
                                selectInput("teams", "Teams", c("All Teams"="",structure(NFL2016stats$Team, names = NFL2016stats$Team )), multiple=TRUE)
@@ -86,25 +102,79 @@ shinyUI(fluidPage(
                       hr(),
                       DT::dataTableOutput("NFLtable")
              ),
+
              
              
-             tabPanel("PlotR",
+             tabPanel("Information", theme = "solarized.css",
+                      sidebarLayout(position = "right",
+                        sidebarPanel(
+                                     h2("Contact Information",style = "color:white"),
+                                     hr(),
+                                     h3("Email:"),
+                                     h3("efn3@hood.edu"),
+                                     h3("GitHub:"),
+                                     h3("elias-nicholson")
+                                     ),
+                        mainPanel(
                       fluidRow(
-                        column(4, 
-                               selectInput("xaxis","X-Axis", vars, selected = "Rank")
-                        ),
-                        column(4, 
-                               selectInput("yaxis", "Y-Axis", vars, selected = "YardsPerGame")
-                        ),
-                        column(4,
-                               radioButtons("cluster","Cluster Choice", choices = list("Conference","Division"), selected = "Division"))
+                        column(8, align = "center", offset = 2,  style = "color:white",
+                               h1("Application Features"))
                       ),
-                      plotOutput("InteractiveScatter")
+                      fluidRow(
+                        column(6, align = "center", offset = 3,
+                               h3("Spatial Mapping"))
+                      ),
+                      fluidRow(
+                        column(8, align = "center", offset = 2,
+                               p("The spatial map included in this app allows users to view the data regionally and take the location into account.
+                                 The primary feature of the page is the map in which the points representing the locations of the teams can be adjusted 
+                                 in size and color. This feature can be accessed via the draggable panel located on the right side of the screen. The second
+                                 feature of this page includes two plots which are reactive to the points which are in view on the screen. The first reactive plot 
+                                 is a scatterplot representing the team ranks versus their points per game. The second of the reactive plots is a barplot which
+                                 increases or decreases dependent upon the number of Superbowl wins the visible teams contain."))
+                      ),
+                      fluidRow(
+                        column(6, align = "center", offset = 3,
+                               h3("Interactive Data Table"))
+                      ),
+                      fluidRow(
+                        column(8, align = "center", offset = 2,
+                               p("The interactive data table feature allows users a few different ways to sort and filter their data.
+                                 The primary method for filtering this data that is used in the app is the selection of the team/s. This can be done 
+                                 by either typing or selecting the team/s which are desired in the dropdown menu. The primary method of sorting that the 
+                                 data table  utiliizes is ascending or descending sorting by any of the variables contatined in the data table.
+                                 This sorting feature can be utilized by clicking the up/down arrows which are located next to each of the variables."))
+                               ),
+                      fluidRow(
+                        column(6, align = "center", offset = 3,
+                               h3("PlotR"))
+                      ),
+                      fluidRow(
+                        column(8, align = "center", offset = 2,
+                               p("The interactive plot has several different ways that the data can be viewed. The first feature of the plot is the choice in the 
+                                 variables plotted. The variables can be selected via dropdown menu. The next feature is adjusting the grouping of the points. Two 
+                                 different forms of grouping available is grouping by conference or grouping by division. The next feature available is the option
+                                 to view trends in two different ways. The first way to view trends is through the linear model; the linear model relays information 
+                                 for each group pertaining to the formula and the R-squared value. The second way to view the trends is through the loess model; the
+                                 loess model which bases its fitting on localized subsets to allow for more accurate modeling of curves."))
+                               ),
+                      fluidRow(
+                        column(8, align = "center", offset = 2,
+                               h5("Acknowledgements: Data gathered from the National Football League, cascading style sheet generated by bootswatch, and map generated by OpenStreetMap"))
+                      )
+                        )
+                      )
+                      
+                      
+                      
              ),
              
              
              conditionalPanel("false", icon("crosshair"))
   )
+  
+  
+  
+  
 )
 )
-
